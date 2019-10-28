395d3b878821ecaee861af4e7792fdcd8ffc4559
Yoonji Kim <yoonji@kaist.ac.kr>
2019-10-28 13:25:38 +0900

    Bugfix move codes for pulse and doafter to setup() from loop()

--- HeyTeddy/arduinoCode/arduinoCode.ino
+++ HeyTeddy/arduinoCode/arduinoCode.ino
@@ -1,11 +1,36 @@
 /*
-	Auto-Generated Code by Protalk
-	2019-07-02T13:19:44.826
+	Auto-Generated Code by HeyTeddy
+	2019-10-24T20:26:20.179
 */
 
 void setup() {
+	pinMode(11, OUTPUT);
+	pinMode(6, OUTPUT);
+	//[3] Pulse D10 every 1.0 seconds 10 times
+	pinMode(10, OUTPUT);
+	int period = 1000.0; 
+	int times = 10; 
+	for(int i=0; i<10; i++) { 
+	  delay(period); 
+	  digitalWrite(10, HIGH);
+	  delay(period); 
+	  digitalWrite(10, LOW);
+	}
+
 
 }
 
 void loop() {
+	//[1] if ( A2 > 300 ) then { [0] Write D11 high }
+	if(analogRead(2) > 300)
+	{
+	//[0] Write D11 high
+	digitalWrite(11, HIGH);
+	}
+	
+	//[2] Pass data from A2 to D6
+	int inputValue = 0;
+	inputValue = analogRead(2);
+	analogWrite(6, inputValue);
+	
 }
